8d5e2668f472766b63372afd87ca239adce6fd1a
Bhavya366 <21b01a12c8@svecw.edu.in>
2023-02-27 22:49:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function countDown(){
+    var hrs=document.getElementById('hr');
+    var mins=document.getElementById('min');
+    var sec=document.getElementById('sec');
+    console.log(hrs.value);
+    setInterval(()=>{
+        console.log("hiii");
+    },1000)
+}
